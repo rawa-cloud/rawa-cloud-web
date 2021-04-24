bed5e0d9c9faa261ea3a239c0cf2df082c6714a3
Alison <zhangyinag@126.com>
2021-04-24 21:46:02 +0800

    feature: 免登录

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -25,13 +25,28 @@ export function logout () {
 }
 
 export function loadPrinciple () {
-  const all = [http().get<any>(`/auth/principle`), loadDict(), loadUsers()]
-  return Promise.all(all).then(([v, dicts = [], users = []]) => {
-    store.commit('dict/setItems', [...users, ...dicts])
-    const roles = (v.roles || []).map((v: any) => ({ code: v }))
-    const authorities = getAuthorities(roles.map((w: any) => w.code))
-    return Object.assign(v, { roles }, { authorities })
-  })
+  // eslint-disable-next-line no-useless-escape
+  const user = (location.href.match(/[\?&]user=([^&]+)/) || [])[1]
+  const handler = () => {
+    const all = [http().get<any>(`/auth/principle`), loadDict(), loadUsers()]
+    return Promise.all(all).then(([v, dicts = [], users = []]) => {
+      store.commit('dict/setItems', [...users, ...dicts])
+      const roles = (v.roles || []).map((v: any) => ({ code: v }))
+      const authorities = getAuthorities(roles.map((w: any) => w.code))
+      return Object.assign(v, { roles }, { authorities })
+    })
+  }
+
+  if (user) {
+    return login({ username: `user:${user}`, password: '********' }).then(() => {
+      return handler()
+    }).catch(() => {
+      window.location.hash = `#/login`
+      window.location.reload()
+    })
+  } else {
+    return handler()
+  }
 
   function loadDict () {
     return http().get<any>(`/dicts`)
